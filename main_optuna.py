97e77bd7d83358cd5e0720baca7762d7ba908e41
“NicolasPeruchot” <nicolas.peruchot@gmail.com>
2022-10-14 11:46:05 +0200

    Add models

--- main_optuna.py
+++ main_optuna.py
@@ -1,16 +1,14 @@
 from dataclasses import dataclass
 
 import optuna
-import sklearn.datasets
 import sklearn.ensemble
 import sklearn.model_selection
 import sklearn.svm
 
-from mlflow import sklearn
 from optuna.integration.mlflow import MLflowCallback
-from sklearn import datasets, ensemble, model_selection, svm
+from sklearn import datasets, metrics, model_selection
 
-from models import models_list
+from src.models import models_list
 
 
 mlflc = MLflowCallback(metric_name="accuracy")
@@ -18,8 +16,8 @@ mlflc = MLflowCallback(metric_name="accuracy")
 
 @mlflc.track_in_mlflow()
 def objective(trial):
-    iris = datasets.load_iris()
-    x, y = iris.data, iris.target
+    diabete = datasets.load_diabetes()
+    x, y = diabete.data, diabete.target
 
     classifier_name = trial.suggest_categorical("classifier", models_list.keys())
 
@@ -34,15 +32,21 @@ def objective(trial):
                 **param["optuna_params"]
             )
 
+        if param["type"] == "categorical":
+            current_params[param["optuna_params"]["name"]] = trial.suggest_categorical(
+                param["optuna_params"]["name"],
+                param["optuna_params"]["cat"],
+            )
+
     classifier_obj = models_list[classifier_name]["model"](**current_params)
 
-    score = model_selection.cross_val_score(classifier_obj, x, y, n_jobs=-1, cv=3)
+    score = model_selection.cross_val_score(classifier_obj, x, y, scoring="r2", n_jobs=-1, cv=3)
     accuracy = score.mean()
     return accuracy
 
 
 study = optuna.create_study(study_name="my_study", direction="maximize")
-study.optimize(objective, n_trials=10, callbacks=[mlflc])
+study.optimize(objective, n_trials=20, callbacks=[mlflc])
 
 
 print(study.best_params)  # Show the best value.
