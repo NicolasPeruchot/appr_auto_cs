1b82c532374c98edb4feec163de5c42f27bea572
“NicolasPeruchot” <nicolas.peruchot@gmail.com>
2022-10-14 09:04:07 +0200

    Refactoring example

---
+++ src/preprocess/preprocess.py
@@ -0,0 +1,18 @@
+def drop_useless(data):
+    data = data.drop(
+        [
+            "Listing Name",
+            "Host ID",
+            "Host Name",
+            "Host Since",
+            "Is Exact Location",
+            "Country",
+            "First Review",
+            "Last Review",
+            "City",
+            "Postal Code",
+            "Country Code",
+        ],
+        axis=1,
+    )
+    return data
