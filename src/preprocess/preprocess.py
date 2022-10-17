60584cbb58b67d250dc6846d052a3a5f43923748
“NicolasPeruchot” <nicolas.peruchot@gmail.com>
2022-10-17 17:05:48 +0200

    Refactor one-hot encoding

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -1,6 +1,9 @@
 from collections import OrderedDict
 
 import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import OneHotEncoder
 
 
 def drop_useless(data):
@@ -25,6 +28,7 @@ def drop_useless(data):
 
 
 def incomplete_columns(data):
+    """Names and informations about features with missing data."""
     total = 0
     list_features = {}
     for col in data.columns:
@@ -41,9 +45,8 @@ def incomplete_columns(data):
 
 
 def min_miss_value_corr(data, listfeatures):
+    """Returns the minimum correlation for features with missing data."""
     tablefeatures = np.ones((len(listfeatures), len(listfeatures)))
-
-    # calcul des corrélations de missing entre deux features
     index1 = 0
     for q in listfeatures:
         q_nan = data[q].isna()
@@ -53,10 +56,25 @@ def min_miss_value_corr(data, listfeatures):
             p_nan = data[p].isna()
             if q != p:
                 bothmiss = sum(q_nan & p_nan)
-                # print ("P("+q+" et "+p+" vaut NaN) =" + str(bothmiss/nanrows))
                 tablefeatures[index1, index2] = bothmiss / nanrows
             index2 += 1
         index1 += 1
         min_corr_pct = round(tablefeatures.min() * 100, 2)
 
     return min_corr_pct
+
+
+class CustomOneHotEncoder(OneHotEncoder):
+    def __init__(self, categories="auto"):
+        super().__init__(categories=categories)
+
+    def fit(self, X, y=None):
+        self.features_to_encode = list(X.select_dtypes(["object"]).columns)
+        return super().fit(X[self.features_to_encode], y)
+
+    def transform(self, X):
+        one_hot_encoded = pd.DataFrame(
+            super().transform(X[self.features_to_encode]).toarray(),
+            columns=self.get_feature_names_out(),
+        )
+        return pd.concat([X.drop(columns=self.features_to_encode), one_hot_encoded], axis=1)
